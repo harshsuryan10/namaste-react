66525c3a07506323531dbb5648c7d2933dda2285
harshsuryan10 <harsh.suryan10@gmail.com>
2024-09-03 21:26:15 +0530

    part-01

---
+++ App.js
@@ -0,0 +1,26 @@
+//Nested create h1 in react
+const parent = React.createElement("div",
+    {id:"parent"}
+    ,[React.createElement("div",{id:"child"},
+        [React.createElement("h1",
+            {},
+            'Hello, world!1'
+        ),React.createElement("h2",
+            {},
+            'Hello, world!2'
+        )]
+    ),
+    React.createElement("div",{id:"child1"},
+        [React.createElement("h1",
+            {},
+            'Hello, world!3'
+        ),React.createElement("h2",
+            {},
+            'Hello, world!4'
+        )]
+    )]
+);
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
