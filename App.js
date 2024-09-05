6eb01ebe2b9313c6045c4e2e025e6f8da99ef09b
harshsuryan10 <harsh.suryan10@gmail.com>
2024-09-06 00:08:34 +0530

    part-04

--- App.js
+++ App.js
@@ -1,6 +1,105 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //part-02
+/*
+Header
+-logo
+-navitems
+Body
+-search
+-Restaurant container
+--restaurant cards
+---img
+---name or res ,star rating,cuisine,delivery time
+Footer
+-copyright
+-links
+-address
+-contact
+*/
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpiCb5hx4mPqXFLWJg85zt0-UgSO7ZFY27Jg&s"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+const styleCard={
+    backgroundColor:"lightgrey"
+}
+
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    const {name,cuisines,avgRating,costfortwo} = resData;
+    return (
+        <div className="res-card" style={styleCard}>
+            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"/>
+            <h3>{name}</h3>
+            <h4>{cuisines}</h4>
+            <h4>{avgRating} Stars</h4>
+            <h4>{costfortwo} rupees</h4>
+            <h5>38mins</h5>
+        </div>
+    );
+}
+const resobj = [
+    {
+        id:"1234",
+        name: "Cafe Amudham",
+        costfortwo: "₹400 for two",
+        cuisines: ["South Indian","Snacks"],
+        avgRating: 4.6,
+    },
+    {
+        id:"1235",
+        name: "Meghna Foods",
+        costfortwo: "₹350 for two",
+        cuisines: ["North Indian","Snacks"],
+        avgRating: 4.5,
+    }
+];
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {
+                    resobj.map(resturant => <RestaurantCard key={resturant.id} resData={resturant}/>)
+                }
+            </div>
+        </div>
+    );
+}
+const AppLayout = () => {
+    return (
+      <div className="app">
+        <Header/>
+        <Body/>
+      </div>  
+    );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
+
+
+
+
+
+
+
+
+
+//part-02
 //React Components
 //two types of components
 //1. Functional Components - new
@@ -11,28 +110,28 @@ import ReactDOM from "react-dom/client";
 //     return true;
 // }
 // const fn2 = () => true;
-const Title = ()=> {
-    return <div>
-                <h1>XYZ Company Title Component</h1>
-            </div>;
-}
-const title =  (
-     <div>
-        <h1>XYZ Company Title Component</h1>
-    </div>
-);
-//Component composition
-const number = 12/0;
-const HeadingComponent = ()=> {
-    return (<div className="container">
-                <Title/> 
-                {title};
-                <h1>{number}</h1>
-                <h1>Heading Component</h1>
-            </div>);
-}
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+// const Title = ()=> {
+//     return <div>
+//                 <h1>XYZ Company Title Component</h1>
+//             </div>;
+// }
+// const title =  (
+//      <div>
+//         <h1>XYZ Company Title Component</h1>
+//     </div>
+// );
+// //Component composition
+// const number = 12/0;
+// const HeadingComponent = ()=> {
+//     return (<div className="container">
+//                 <Title/> 
+//                 {title};
+//                 <h1>{number}</h1>
+//                 <h1>Heading Component</h1>
+//             </div>);
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponent/>);
 //React element => Object =>(after render) HTML-element
 // const heading = React.createElement("h1",{id:"heading"},"Namaste React")
 // const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -44,22 +143,6 @@ root.render(<HeadingComponent/>);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 // root.render(jsxHeading);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
